071ea526ce036503b1e2a1fc2be96dbf8992b1a9
Pavithran <pavithrank02@gmail.com>
2022-08-20 23:02:53 +0530

    REST Api fixes

---
+++ script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v3.1/name/india');
+request.send();
+console.log(request.responseText)
+
+request.addEventListener('load', function() {
+    console.log(this.responseText);
+    const [data] = JSON.parse(this.responseText);
+    //const data = dataArray[0]
+    console.log('data', data);
+    console.log('data', data.languages['eng']);
+
+    const html = `
+    <article class="country">
+          <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages['eng']}</p>
+            <p class="country__row"><span>💰</span>${data.currencies['USD'].name}</p>
+          </div>
+        </article>
+    `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+})
